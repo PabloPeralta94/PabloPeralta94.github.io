a75999854ae36e353cb0ecdd532bfe656454af33
PabloPeralta94 <113708626+PabloPeralta94@users.noreply.github.com>
2022-12-26 14:07:27 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("java");
+        habilidades[2].classList.add("php");
+        habilidades[3].classList.add("android");
+        habilidades[4].classList.add("sqlserver");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+//animaciones intereses
+
+const link = document.querySelectorAll('.link');
+const linkHoverReveal = document.querySelectorAll('.hover-reveal');
+const linkImages = document.querySelectorAll('.hidden-img');
+
+
+for(let i = 0; i < link.length; i++) {
+  link[i].addEventListener('mousemove', (e) => {
+    linkHoverReveal[i].style.opacity = 1;
+    linkHoverReveal[i].style.transform = `translate(-100%, -50% ) rotate(5deg)`;
+    linkImages[i].style.transform = 'scale(1, 1)';
+    linkHoverReveal[i].style.left = e.clientX;
+  })
+  
+  link[i].addEventListener('mouseleave', (e) => {
+    linkHoverReveal[i].style.opacity = 0;
+    linkHoverReveal[i].style.transform = `translate(-50%, -50%) rotate(-5deg)`;
+    linkImages[i].style.transform = 'scale(0.8, 0.8)';
+  })
+}
+
+//Boton de descargar CV a modo de ejemplo (Solo muestra un mensaje)
+
+const botoncv = document.getElementById("botonCv");
+botoncv.onclick = function() {
+    alert("El cv del cliente ha sido descargado")
+}
+
+//Form contactame
+const $form = document.querySelector('#form')
+    const $buttonMailto = document.querySelector('#enviarMail')
+
+    $form.addEventListener('submit', handleSubmit)
+
+    function handleSubmit(event) {
+      event.preventDefault()
+      const form = new FormData(this)
+      $buttonMailto.setAttribute('href', `mailto:zachary.henderson@example.com?subject=nombre ${form.get('name')}  asunto ${form.get('asunto')}&body=${form.get('message')}`)
+      $buttonMailto.click()
+    }
+
+//el email no sera entregado puesto que el correo es a modo de ejemplo
